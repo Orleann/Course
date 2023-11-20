d1d8d1c3b447c14ac057af7f65e5217af30fea0d
SebastianS <seb.kontakt.sob@gmail.com>
2023-11-20 12:20:42 +0100

    okok?

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -27,6 +27,35 @@ const Total = ({ parts }) => {
   );
 };
 
+import React from 'react';
+
+// Header component
+const Header = ({ course }) => {
+  return (
+    <h1>{course}</h1>
+  );
+};
+
+// Content component
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map((part, index) => (
+        <Part key={index} part={part.name} exercises={part.exercises} />
+      ))}
+    </div>
+  );
+};
+
+// Total component
+const Total = ({ parts }) => {
+  const totalExercises = parts.reduce((total, part) => total + part.exercises, 0);
+
+  return (
+    <p><strong>Total exercises: {totalExercises}</strong></p>
+  );
+};
+
 const App = () => {
   const course = {
     name: 'Half Stack application development',

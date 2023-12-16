cdac5ed96418c893ed5474a34e8ec3595c1aa7b5
Orleann <seb.kontakt.sob@gmail.com>
2023-12-16 12:08:12 +0100

    Exercise 1.4

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -10,7 +10,7 @@ const Header = (props) => {
 const Part = ({ name, exercises }) => {
   return (
     <p>
-      {name} - {exercises}
+      {name} {exercises}
     </p>
   );
 };
@@ -34,21 +34,19 @@ const Total = ({ parts }) => {
 
 const App = () => {
   const course = 'Half Stack application development';
-
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  };
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  };
   const part3 = {
     name: 'State of a component',
     exercises: 14
   };
 
-  const parts = [part1, part2, part3];
+  const parts = [
+    {name: 'Fundamentals of React',
+    exercises: 10},
+    {name: 'Using props to pass data',
+    exercises: 7},
+    {name: 'State of a component',
+    exercises: 14}
+  ];
 
   return (
     <div>

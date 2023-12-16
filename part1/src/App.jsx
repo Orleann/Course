a0cc0181bf0a1b05ec8bddb1a3b5b1f0acb6a2b0
Orleann <seb.kontakt.sob@gmail.com>
2023-12-16 12:38:00 +0100

    Step2 done

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,22 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 
 const App = () => {
-  // State variables to store counts for good, neutral, and bad feedback
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
   const handleGoodFeedback = () => {
-    setGood(good + 1);
-  };
+    setGood(good + 1)
+  }
 
   const handleNeutralFeedback = () => {
-    setNeutral(neutral + 1);
-  };
+    setNeutral(neutral + 1)
+  }
 
   const handleBadFeedback = () => {
-    setBad(bad + 1);
-  };
+    setBad(bad + 1)
+  }
+
+  const all = good + bad + neutral
+  const average = (good * 1 + neutral * 0 + bad * -1) / all
+  const postitive = good / all
 
   return (
     <div>
@@ -25,10 +28,13 @@ const App = () => {
       <button onClick={handleNeutralFeedback}>Neutral</button>
       <button onClick={handleBadFeedback}>Bad</button>
 
-      <h2>Statistics</h2>
-      <p>Good: {good}</p>
-      <p>Neutral: {neutral}</p>
-      <p>Bad: {bad}</p>
+      <h2>statistics</h2>
+      <p>good: {good}</p>
+      <p>neutral: {neutral}</p>
+      <p>bad: {bad}</p>
+      <p>all: {all}</p>
+      <p>average: {average}</p>
+      <p>postitive: {postitive}</p>
     </div>
   );
 };

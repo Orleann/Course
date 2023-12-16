3ac7aa8e9ac1a95670467f17ea3e59bae123f55d
Orleann <seb.kontakt.sob@gmail.com>
2023-12-16 12:24:28 +0100

    Step1 done

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,16 +1,36 @@
-import { useState } from 'react'
+import React, { useState } from 'react';
 
 const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  // State variables to store counts for good, neutral, and bad feedback
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const handleGoodFeedback = () => {
+    setGood(good + 1);
+  };
+
+  const handleNeutralFeedback = () => {
+    setNeutral(neutral + 1);
+  };
+
+  const handleBadFeedback = () => {
+    setBad(bad + 1);
+  };
 
   return (
     <div>
-      code here
-    </div>
-  )
-}
+      <h1>Give Feedback</h1>
+      <button onClick={handleGoodFeedback}>Good</button>
+      <button onClick={handleNeutralFeedback}>Neutral</button>
+      <button onClick={handleBadFeedback}>Bad</button>
 
-export default App
+      <h2>Statistics</h2>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
+    </div>
+  );
+};
+
+export default App;

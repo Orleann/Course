41dbe41f056ad64a5f345a9cf10b53ee7c31b365
Orleann <seb.kontakt.sob@gmail.com>
2023-12-17 15:04:39 +0100

    Exercise 1.13 done

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,11 +1,5 @@
 import React, { useState } from 'react'
 
-const Button = ({ handleCLick, text}) => {
-  return(
-    <button onClick={handleCLick}>{text}</button>
-  )
-}
-
 const StatisticsLine = ({text, value}) => {
   return(
     <tr>
@@ -33,12 +27,28 @@ const Statistics = ({good, neutral, bad}) => {
   )
 }
 
+const Votes = {
+    0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0   
+  }
+
+const Anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+
 const App = () => {
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
-  const [hasFeedback, setHasFeedback] = useState(0);
-  const [selected, setSelected] = useState(0);
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const [hasFeedback, setHasFeedback] = useState(0)
+  const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(Votes)
 
   const handleGoodFeedback = () => {
     setGood(good + 1)
@@ -55,23 +65,17 @@ const App = () => {
     setHasFeedback(true)
   }
 
-  const handleSelected = () => {
-    setSelected(Math.floor(Math.random() * (Anecdotes.length)))
+  const handleVotes = () => {
+    const newVotes = { ...votes }
+    newVotes[selected] += 1
+    setVotes(newVotes)
   }
 
-  const Anecdotes = [
-    'If it hurts, do it more often.',
-    'Adding manpower to a late software project makes it later!',
-    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
-    'The only way to go fast, is to go well.'
-  ]
-
+  const handleSelected = () => {
+    const randomIndex = Math.floor(Math.random() * Anecdotes.length);
+    setSelected(randomIndex);
+  }
   
-
   return (
     <div>
       <h1>Give Feedback</h1>
@@ -82,6 +86,8 @@ const App = () => {
 
       {hasFeedback && <Statistics good={good} neutral={neutral} bad={bad} /> || <p>No feedback given</p>}
       {Anecdotes[selected]}
+      <p>has {votes[selected]} votes</p>
+      <p><button onClick={handleVotes}>vote</button></p>
     </div>
   );
 };

23586ac3f2090ac0a11d026c874d0f5bf6dd816c
Orleann <seb.kontakt.sob@gmail.com>
2024-02-11 10:18:47 +0100

    better

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -5,7 +5,13 @@ import Persons from './Persons';
 import services from './services';
 
 const App = () => {
-  const [persons, setPersons] = useState([]);
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '123-456-7890' },
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [searchTerm, setSearchTerm] = useState('');

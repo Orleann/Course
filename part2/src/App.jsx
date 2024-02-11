f1e0c72769b10d265ae31e93e268bdf4e72a7e95
Orleann <seb.kontakt.sob@gmail.com>
2024-02-11 09:38:54 +0100

    2.8 done

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -1,8 +1,11 @@
 import React, { useState } from 'react';
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]);
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '123-456-7890' }
+  ]);
   const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
 
   const addPerson = (event) => {
     event.preventDefault();
@@ -10,12 +13,14 @@ const App = () => {
     if (persons.some(person => person.name.toLowerCase() === newName.toLowerCase())) {
       alert(`${newName} is already added to the phonebook`);
       setNewName('');
+      setNewNumber('');
       return;
     }
 
-    const newPerson = { name: newName };
+    const newPerson = { name: newName, number: newNumber };
     setPersons([...persons, newPerson]);
     setNewName('');
+    setNewNumber('');
   };
 
   return (
@@ -26,14 +31,16 @@ const App = () => {
           name: <input value={newName} onChange={(e) => setNewName(e.target.value)} />
         </div>
         <div>
+          number: <input value={newNumber} onChange={(e) => setNewNumber(e.target.value)} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {/* Render the list of persons */}
       <ul>
         {persons.map((person, index) => (
-          <li key={index}>{person.name}</li>
+          <li key={index}>{person.name} - {person.number}</li>
         ))}
       </ul>
     </div>
